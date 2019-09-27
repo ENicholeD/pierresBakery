2b538ece92ea80d189b97c9bc460617510c3771b
Erin Deuel <end.gaia@gmail.com>
2019-09-27 12:03:00 -0700

    do step by stem to fix the bread and pastry options

--- bakery.cs
+++ bakery.cs
@@ -5,17 +5,21 @@ namespace PierresBakery
 {
     public class Bakery
     {
-        public string Deal;
+        public string Offer;
         public int Price;
     }
-    public class Items
+    public class Program
     {
         public static void Main()
         {
-            Bakery singleBread = new Bakery("One baguette", 5);
-            Bakery breadDeal = new Bakery("Buy two get one free bread!", 10);
+            Bakery singleBread = new Bakery();
+            singleBread.Offer = "One baguette";
+            singleBread.Price = 5;
+            Bakery breadDeal = new Bakery();
+            breadDeal.Offer = "Buy two get one free bread!";
+            breadDeal.Price = 10;
             Bakery singlePastry = new Bakery("One Pastry", 2);
-            Bakery threePastries = new Bakery("3 for $5 pastries", 5)
+            Bakery threePastries = new Bakery("3 for $5 pastries", 5);
 
             Console.WriteLine("Welcome to Pierre's Bakery! I am currently having a sale! one baguette for $5 or buy two get one free. I also have a sale on pastries, one for $2 or 3 for $5! What can I get you today? (b, 3b, pm 3p)");
             string customerChoice = Console.ReadLine();
