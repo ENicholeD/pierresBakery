7622b6eee1da1da70afa75adf2f634419afb819f
Erin Deuel <end.gaia@gmail.com>
2019-09-27 14:32:24 -0700

    add ascii art

--- bakery.cs
+++ bakery.cs
@@ -29,6 +29,31 @@ namespace PierresBakery
             threePastries.Offer = "3 pastries";
             threePastries.Price = 5;
 
+            var arr = new[]
+							{   @" ____________________________________________ ",
+                                @"|               Pierre's Bakery              |",
+                                @"|____________________________________________|",
+                                @"|__||  ||___||  |_|___|___|__|  ||___||  ||__|",
+                                @"||__|  |__|__|  |___|___|___||  |__|__|  |__||",
+                                @"|__||  ||___||  |_|___|___|__|  ||___||  ||__|",
+                                @"||__|  |__|__|  |    || |    |  |__|__|  |__||",
+                                @"|__||  ||___||  |    || |    |  ||___||  ||__|",
+                                @"||__|  |__|__|  |    || |    |  |__|__|  |__||",
+                                @"|__||  ||___||  |    || |    |  ||___||  ||__|",
+                                @"||__|  |__|__|  |    || |    |  |__|__|  |__||",
+                                @"|__||  ||___||  |   O|| |O   |  ||___||  ||__|",
+                                @"||__|  |__|__|  |    || |    |  |__|__|  |__||",
+                                @"|__||  ||___||  |    || |    |  ||___||  ||__|",
+                                @"||__|  |__|__|__|____||_|____|  |__|__|  |__||",
+                                @"|LLL|  |LLLLL|______________||  |LLLLL|  |LLL|",
+                                @"|LLL|  |LLL|______________|  |  |LLLLL|  |LLL|",
+                                @"|LLL|__|L|______________|____|__|LLLLL|__|LLL|",   
+							};
+
+				Console.WriteLine("\n\n");
+				foreach(string line in arr )
+				Console.WriteLine(line);
+
             Console.WriteLine("Welcome to Pierre's Bakery! I am currently having a sale! one baguette for $5 or buy two get one free. I also have a sale on pastries, one for $2 or 3 for $5! What can I get you today? (b, 3b, p, 3p)");
             string customerChoice = Console.ReadLine();
             Console.WriteLine(customerChoice);
