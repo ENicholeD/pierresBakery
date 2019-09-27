4e109b0af542f9026741a2654be96367165e7cb4
Erin Deuel <end.gaia@gmail.com>
2019-09-27 12:20:15 -0700

    get bread and pastry items to work

--- bakery.cs
+++ bakery.cs
@@ -15,15 +15,37 @@ namespace PierresBakery
             Bakery singleBread = new Bakery();
             singleBread.Offer = "One baguette";
             singleBread.Price = 5;
-            Bakery breadDeal = new Bakery();
-            breadDeal.Offer = "Buy two get one free bread!";
-            breadDeal.Price = 10;
-            Bakery singlePastry = new Bakery("One Pastry", 2);
-            Bakery threePastries = new Bakery("3 for $5 pastries", 5);
 
-            Console.WriteLine("Welcome to Pierre's Bakery! I am currently having a sale! one baguette for $5 or buy two get one free. I also have a sale on pastries, one for $2 or 3 for $5! What can I get you today? (b, 3b, pm 3p)");
+            Bakery breadDeal = new Bakery();
+            breadDeal.Offer = "Buy two get one free bread";
+            breadDeal.Price = 10;
+
+            Bakery singlePastry = new Bakery();
+            singlePastry.Offer = "One pastry";
+            singlePastry.Price = 2;
+            
+            Bakery threePastries = new Bakery();
+            threePastries.Offer = "3 for $5 pastries";
+            threePastries.Price = 5;
+
+            Console.WriteLine("Welcome to Pierre's Bakery! I am currently having a sale! one baguette for $5 or buy two get one free. I also have a sale on pastries, one for $2 or 3 for $5! What can I get you today? (b, 3b, p, 3p)");
             string customerChoice = Console.ReadLine();
             Console.WriteLine(customerChoice);
+
+            if(customerChoice == "b")
+            {
+                Console.WriteLine(singleBread.Offer + " for $" + singleBread.Price);
+            }
+
+            if (customerChoice == "3b")
+            {
+                Console.WriteLine(breadDeal.Offer + " for $" + breadDeal.Price);
+            }
+
+            if (customerChoice == "p")
+            {
+                Console.WriteLine(singlePastry.Offer + " for $" + singlePastry.Price);
+            }
         }
     }
 }
